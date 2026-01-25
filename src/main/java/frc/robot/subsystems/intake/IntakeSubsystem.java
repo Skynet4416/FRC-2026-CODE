a76667c95d77479eb7cbb9139e1fc40abb9f00f1
romyrahav-hash <romyrahav@gmail.com>
2026-01-25 21:32:22 +0200

    Added LoggedTunableNumber object

--- src/main/java/frc/robot/subsystems/intake/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeSubsystem.java
@@ -1,7 +1,13 @@
 package frc.robot.subsystems.intake;
 
-public class IntakeSubsystem {
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.util.LoggedTunableNumber;
+
+public class IntakeSubsystem extends SubsystemBase {
   private final IntakeSubsystemIO io;
+  private final LoggedTunableNumber targetRpm = new LoggedTunableNumber("Intake", 500.0);
   protected final IntakeIOInputsAutoLogged inputs = new IntakeIOInputsAutoLogged();
 
   public IntakeSubsystem(IntakeSubsystemIO io) {
