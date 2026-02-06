f1ff515f31b8104b544f9a35a4aa21a49041b2fa
IdanKahan <idankahan14@gmail.com>
2026-02-06 21:19:06 +0200

    Added flywheel sysid

--- src/main/java/frc/robot/subsystems/drive/shooter/FlywheelSubsystem.java
+++ src/main/java/frc/robot/subsystems/drive/shooter/FlywheelSubsystem.java
@@ -1,19 +1,32 @@
 package frc.robot.subsystems.drive.shooter;
 
+import static edu.wpi.first.units.Units.Volts;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import frc.robot.util.LoggedTunableNumber;
 import org.littletonrobotics.junction.Logger;
 
 public class FlywheelSubsystem extends SubsystemBase {
   private final FlywheelSubsystemIO io;
+  private final SysIdRoutine sysIdRoutine;
   private final LoggedTunableNumber targetRpm = new LoggedTunableNumber("FlywheelRPM", 4500.0);
 
   protected final FlywheelIOInputsAutoLogged inputs = new FlywheelIOInputsAutoLogged();
 
   public FlywheelSubsystem(FlywheelSubsystemIO io) {
     this.io = io;
+    sysIdRoutine =
+        new SysIdRoutine(
+            new SysIdRoutine.Config(
+                null,
+                null,
+                null,
+                (state) -> Logger.recordOutput("Flywheel/SysIdTestState", state.toString())),
+            new SysIdRoutine.Mechanism(
+                (voltage) -> runVolts(voltage.in(Volts)), null, this));
   }
 
   public void setTargetRPM(double rpm) {
@@ -46,6 +59,14 @@ public class FlywheelSubsystem extends SubsystemBase {
 
   public Command runFlywheelCommand() {
     return Commands.runEnd(() -> setTargetRPM(targetRpm.get()), () -> stop(), this);
+  }
+
+  public Command sysIdQuasistatic(SysIdRoutine.Direction direction) {
+    return sysIdRoutine.quasistatic(direction);
+  }
+
+  public Command sysIdDynamic(SysIdRoutine.Direction direction) {
+    return sysIdRoutine.dynamic(direction);
   }
 
   @Override
