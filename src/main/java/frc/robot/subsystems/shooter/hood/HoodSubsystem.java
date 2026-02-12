7b097a9f988b79ff5e9ef1370cd8c4dc285bcd7c
IdanKahan <idankahan14@gmail.com>
2026-02-13 00:40:37 +0200

    Added setpoint mech

--- src/main/java/frc/robot/subsystems/shooter/hood/HoodSubsystem.java
+++ src/main/java/frc/robot/subsystems/shooter/hood/HoodSubsystem.java
@@ -2,12 +2,17 @@ package frc.robot.subsystems.shooter.hood;
 
 import static edu.wpi.first.units.Units.Volts;
 
+import edu.wpi.first.wpilibj.util.Color;
+import edu.wpi.first.wpilibj.util.Color8Bit;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import frc.robot.Constants;
 import frc.robot.util.LoggedTunableNumber;
 import org.littletonrobotics.junction.Logger;
+import org.littletonrobotics.junction.mechanism.LoggedMechanism2d;
+import org.littletonrobotics.junction.mechanism.LoggedMechanismLigament2d;
+import org.littletonrobotics.junction.mechanism.LoggedMechanismRoot2d;
 
 public class HoodSubsystem extends SubsystemBase {
   private final HoodSubsystemIO io;
@@ -16,6 +21,21 @@ public class HoodSubsystem extends SubsystemBase {
 
   private final LoggedTunableNumber targetAngle = new LoggedTunableNumber("Hood/TargetAngle", 0.0);
   private final LoggedTunableNumber zeroWait = new LoggedTunableNumber("Hood/ZeroWait", 0.5);
+
+  private final LoggedMechanism2d mech =
+      new LoggedMechanism2d(0.5, 0.5, new Color8Bit(Color.kBlack));
+  private final LoggedMechanismRoot2d root = mech.getRoot("HoodPivot", 0.1, 0.1);
+  private final LoggedMechanismLigament2d hood =
+      root.append(
+          new LoggedMechanismLigament2d("Hood", 0.091, 0, 10, new Color8Bit(Color.kOrange)));
+
+  private final LoggedMechanism2d mechSetpoint =
+      new LoggedMechanism2d(0.5, 0.5, new Color8Bit(Color.kBlack));
+  private final LoggedMechanismRoot2d rootSetpoint =
+      mechSetpoint.getRoot("HoodPivot", 0.1, 0.1);
+  private final LoggedMechanismLigament2d hoodSetpoint =
+      rootSetpoint.append(
+          new LoggedMechanismLigament2d("Hood", 0.091, 0, 10, new Color8Bit(Color.kGreen)));
 
   public HoodSubsystem(HoodSubsystemIO io) {
     this.io = io;
@@ -33,6 +53,12 @@ public class HoodSubsystem extends SubsystemBase {
   public void periodic() {
     io.updateInputs(inputs);
     Logger.processInputs("Hood", inputs);
+
+    hood.setAngle(inputs.angle);
+    Logger.recordOutput("Hood/Mechanism", mech);
+
+    hoodSetpoint.setAngle(inputs.targetAngle);
+    Logger.recordOutput("Hood/SetpointMechanism", mechSetpoint);
   }
 
   public void setTargetAngle(double degrees) {
