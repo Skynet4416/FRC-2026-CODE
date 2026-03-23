42dcffe45562d3689cc5406828364b7de3fe5fb2
IdanKahan <124575412+IdanKahan@users.noreply.github.com>
2026-03-23 12:09:36 +0200

    Added Physics shoot on move (#10)
    * Added physics based shooting
    
    * Updated constants to match LUT
    
    * Updated sim shoot to be same as IRL
    
    * Returned missing Constant
    
    * started shooter tuning
    
    * Fixed wrong id's + added gyro reset (Idan)
    
    * Calibration
    
    * Added hood angle to loadLUTEntry
    
    * Updated Launch calculator to use updated vars
    
    * Made the Max Speed Limit PhysicsAware
    
    * Remoed unused var
    
    * Added shooting confidence gate
    
    * Removed unused var
    
    * Fixed build error (reverted)
    
    * Failed tuning attempt
    
    * Changed transformVelocity
    
    * Tuned shooter to low ceiling
    
    * Shoot On the Move Working
    
    * Updated slip factor in sim
    
    ---------
    
    Co-authored-by: Yotam201410 <yotam201410@gmail.com>

--- src/main/java/frc/robot/subsystems/vision/VisionIOLimelight.java
+++ src/main/java/frc/robot/subsystems/vision/VisionIOLimelight.java
@@ -16,6 +16,7 @@ package frc.robot.subsystems.vision;
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.networktables.DoubleArrayPublisher;
 import edu.wpi.first.networktables.DoubleArraySubscriber;
@@ -39,13 +40,15 @@ public class VisionIOLimelight implements VisionIO {
   private final DoubleArraySubscriber megatag1Subscriber;
   private final DoubleArraySubscriber megatag2Subscriber;
 
+  private final Transform3d offset;
+
   /**
    * Creates a new VisionIOLimelight.
    *
    * @param name The configured name of the Limelight.
    * @param rotationSupplier Supplier for the current estimated rotation, used for MegaTag 2.
    */
-  public VisionIOLimelight(String name, Supplier<Rotation2d> rotationSupplier) {
+  public VisionIOLimelight(String name, Supplier<Rotation2d> rotationSupplier, Transform3d offset) {
     var table = NetworkTableInstance.getDefault().getTable(name);
     this.rotationSupplier = rotationSupplier;
     orientationPublisher = table.getDoubleArrayTopic("robot_orientation_set").publish();
@@ -55,6 +58,7 @@ public class VisionIOLimelight implements VisionIO {
     megatag1Subscriber = table.getDoubleArrayTopic("botpose_wpiblue").subscribe(new double[] {});
     megatag2Subscriber =
         table.getDoubleArrayTopic("botpose_orb_wpiblue").subscribe(new double[] {});
+    this.offset = offset;
   }
 
   @Override
@@ -90,7 +94,8 @@ public class VisionIOLimelight implements VisionIO {
               // 3D pose estimate
               parsePose(rawSample.value),
 
-              // Ambiguity, using only the first tag because ambiguity isn't applicable for multitag
+              // Ambiguity, using only the first tag because ambiguity isn't applicable for
+              // multitag
               rawSample.value.length >= 18 ? rawSample.value[17] : 0.0,
 
               // Tag count
@@ -102,31 +107,31 @@ public class VisionIOLimelight implements VisionIO {
               // Observation type
               PoseObservationType.MEGATAG_1));
     }
-    for (var rawSample : megatag2Subscriber.readQueue()) {
-      if (rawSample.value.length == 0) continue;
-      for (int i = 11; i < rawSample.value.length; i += 7) {
-        tagIds.add((int) rawSample.value[i]);
-      }
-      poseObservations.add(
-          new PoseObservation(
-              // Timestamp, based on server timestamp of publish and latency
-              rawSample.timestamp * 1.0e-6 - rawSample.value[6] * 1.0e-3,
+    // for (var rawSample : megatag2Subscriber.readQueue()) {
+    //   if (rawSample.value.length == 0) continue;
+    //   for (int i = 11; i < rawSample.value.length; i += 7) {
+    //     tagIds.add((int) rawSample.value[i]);
+    //   }
+    //   poseObservations.add(
+    //       new PoseObservation(
+    //           // Timestamp, based on server timestamp of publish and latency
+    //           rawSample.timestamp * 1.0e-6 - rawSample.value[6] * 1.0e-3,
 
-              // 3D pose estimate
-              parsePose(rawSample.value),
+    //           // 3D pose estimate
+    //           parsePose(rawSample.value),
 
-              // Ambiguity, zeroed because the pose is already disambiguated
-              0.0,
+    //           // Ambiguity, zeroed because the pose is already disambiguated
+    //           0.0,
 
-              // Tag count
-              (int) rawSample.value[7],
+    //           // Tag count
+    //           (int) rawSample.value[7],
 
-              // Average tag distance
-              rawSample.value[9],
+    //           // Average tag distance
+    //           rawSample.value[9],
 
-              // Observation type
-              PoseObservationType.MEGATAG_2));
-    }
+    //           // Observation type
+    //           PoseObservationType.MEGATAG_2));
+    // }
 
     // Save pose observations to inputs object
     inputs.poseObservations = new PoseObservation[poseObservations.size()];
@@ -143,14 +148,15 @@ public class VisionIOLimelight implements VisionIO {
   }
 
   /** Parses the 3D pose from a Limelight botpose array. */
-  private static Pose3d parsePose(double[] rawLLArray) {
+  private Pose3d parsePose(double[] rawLLArray) {
     return new Pose3d(
-        rawLLArray[0],
-        rawLLArray[1],
-        rawLLArray[2],
-        new Rotation3d(
-            Units.degreesToRadians(rawLLArray[3]),
-            Units.degreesToRadians(rawLLArray[4]),
-            Units.degreesToRadians(rawLLArray[5])));
+            rawLLArray[0],
+            rawLLArray[1],
+            rawLLArray[2],
+            new Rotation3d(
+                Units.degreesToRadians(rawLLArray[3]),
+                Units.degreesToRadians(rawLLArray[4]),
+                Units.degreesToRadians(rawLLArray[5])))
+        .transformBy(this.offset);
   }
 }
