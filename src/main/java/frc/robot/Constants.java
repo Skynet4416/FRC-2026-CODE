38f2e4d3967ee448e8db083f88af36285fcbff71
Yotam201410 <yotam201410@gmail.com>
2026-06-29 13:59:37 +0300

    Increased intake supply limit

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -240,7 +240,7 @@ public final class Constants {
         // Highly recommended for Shooters.
         public static final boolean SUPPLY_ENABLED = true;
 
-        public static final double SUPPLY_LIMIT_AMPS = 80; // Holding limit
+        public static final double SUPPLY_LIMIT_AMPS = 100; // Holding limit
 
         // Stator Limit: Protects the Motor (Prevents Burnout)
         // Keep this HIGH for Shooters to allow fast spin-up.
